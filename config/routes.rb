0793f3d9a4dc716211f88f7cd5b7668a9b18e207
s12chung <steve.chung7@gmail.com>
2013-12-29 16:43:45 -0500

    basic setup for website

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ PhotoBlog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
+  resources :posts, only: %i[show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
