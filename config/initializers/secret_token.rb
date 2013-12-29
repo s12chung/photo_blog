d6bc78dc2605844af96ca9172edeb1500f441983
s12chung <steve.chung7@gmail.com>
2013-12-29 14:52:29 -0500

    project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoBlog::Application.config.secret_key_base = '1d05217ab56477f0c299df82e6e65dfa1df027a0a4715e66660ce6f1ead01f59a202d254c902bf666f78a84734a5f5c310ce61055b471d88799a6659f1562591'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhotoBlog::Application.config.session_store :cookie_store, key: '_photo_blog_session'
