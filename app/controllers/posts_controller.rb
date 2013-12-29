0a9341fb38ee115990eb53760a8469b126e46ad4
s12chung <steve.chung7@gmail.com>
2013-12-29 18:48:00 -0500

    check auth for edit view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all * 10
   end
+
+  def show
+  end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ PhotoBlog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :posts, only: %i[show]
+  resources :posts, only: %i[show edit]
   controller :session do
     get '/login' => :new
     post '/login' => :create
