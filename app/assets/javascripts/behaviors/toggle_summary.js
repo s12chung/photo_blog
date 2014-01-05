efb2fb01e7010a8eb8d8dac1a02d404d55cd7238
s12chung <steve.chung7@gmail.com>
2014-01-05 11:56:12 -0500

    toggle_summary change

--- app/assets/javascripts/behaviors/toggle_summary.js
+++ app/assets/javascripts/behaviors/toggle_summary.js
@@ -1,7 +1,19 @@
-load_behavior(function() {
-    $(data_behavior('toggle_summary')).click(function(e) {
-        e.preventDefault();
-        var $summary = $(this);
-        $summary.css({ opacity: $summary.css('opacity') === "0" ? 1 : 0 });
+(function() {
+    var visibile;
+
+    load_behavior(function() {
+        var $summary = $(data_behavior('summary_content'));
+
+        if (!defined(visibile)) { visibile = true; }
+        var toggle = function() {
+            visibile ? $summary.show() : $summary.hide();
+        };
+
+        toggle();
+        $(data_behavior('toggle_summary')).click(function(e) {
+            e.preventDefault();
+            visibile = !visibile;
+            $summary.fadeTo(400, visibile ? 1 : 0, toggle);
+        });
     });
-});
+})();
