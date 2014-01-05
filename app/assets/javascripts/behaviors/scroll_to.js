84e687b18169e633c23fc6149f7a05454cc3cd2c
s12chung <steve.chung7@gmail.com>
2014-01-05 11:02:09 -0500

    summary scroll, simplify photo box and background to summary

---
+++ app/assets/javascripts/behaviors/scroll_to.js
@@ -0,0 +1,7 @@
+load_behavior(function() {
+    $(data_behavior("scroll_to")).click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $.scrollTo($(this).data('scroll-to'), 400, { easing: '' });
+    });
+});
