e4cc42203881c584524c0dd2b2d57a24dccc17f0
s12chung <steve.chung7@gmail.com>
2014-01-19 16:52:48 -0500

    make popup work properly for mobile

--- app/assets/javascripts/behaviors/show_popup.js
+++ app/assets/javascripts/behaviors/show_popup.js
@@ -1,15 +1,19 @@
 $(function() {
     $('body').on('click', data_behavior('show_popup'), function(e) {
+        photoswipe.removeEventHandlers();
+
         e.preventDefault();
         var $popup = $(data_behavior('popup'));
         $popup.addClass('visible');
 
         $(data_behavior('close_x')).click(function(e) {
+            photoswipe.addEventHandlers();
+
             e.preventDefault();
             $popup.removeClass('visible');
 
             var $close_x = $popup.children().first();
-            $popup.empty().append($close_x)
+            $popup.empty().append($close_x);
         });
 
         $.get($(this).prop('href'));
