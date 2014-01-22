d6ad1e975a608e6b804f91842c9ddc0adbc7ceac
s12chung <steve.chung7@gmail.com>
2014-01-21 20:22:44 -0500

    make links in post point to new window

--- app/presenters/post_presenter.rb
+++ app/presenters/post_presenter.rb
@@ -33,7 +33,9 @@ module PostPresenter
       markdown_options = %w/autolink no_intra_emphasis/
       renderer_options = %w/hard_wrap/
       renderer = Redcarpet::Markdown.new(
-          Redcarpet::Render::HTML.new(Hash[renderer_options.map {|o| [o.to_sym, true]}]),
+          Redcarpet::Render::HTML.new(Hash[renderer_options.map {|o| [o.to_sym, true]}].merge(
+                                          link_attributes: { target: "_blank" }
+                                      )),
           Hash[markdown_options.map {|o| [o.to_sym, true]}])
       renderer.render(markdown).html_safe
     end
