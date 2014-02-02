b6ed8a9a9e57790f75bcedd2abfa185bceb38f89
s12chung <steve.chung7@gmail.com>
2014-02-01 23:36:27 -0500

    mobile scroll fixes

--- app/assets/javascripts/behaviors/scroll_to.js
+++ app/assets/javascripts/behaviors/scroll_to.js
@@ -2,10 +2,8 @@ $(function() {
     $('body').on('click', data_behavior('scroll_to'), function(e) {
         e.preventDefault();
         e.stopPropagation();
-
-        var $this = $(this);
-        var $scroller = $this.closest(data_behavior("scroller"));
-        $scroller = $scroller.length > 0 ? $scroller : $;
-        $scroller.scrollTo($this.data('scroll-to'), 400);
+        var options = { offset: { top: -5 }};
+        if (is_mobile) options.offset.top += -40;
+        $.scrollTo($(this).data('scroll-to'), 400, options);
     });
 });
