08a9e39f827f141b393efac457dd089f7dc7dd20
s12chung <steve.chung7@gmail.com>
2014-02-02 20:40:08 -0500

    fix popup

--- app/assets/javascripts/behaviors/show_popup.js
+++ app/assets/javascripts/behaviors/show_popup.js
@@ -1,22 +1,21 @@
 $(function() {
+    var $popup = $(data_behavior('popup'));
+    var $content_child = $(data_behavior('popup_content')).children().first();
+
     $('body').on('click', data_behavior('show_popup'), function(e) {
         e.preventDefault();
-        var $popup = $(data_behavior('popup'));
+        $content_child.show();
         $popup.css({ opacity: 1, visibility: 'visible' });
 
         $(data_behavior('close_x')).click(function(e) {
-            $('body').scrollTop(0);
-
             e.preventDefault();
             $popup.css({ opacity: 0 });
 
             setTimeout(function() {
                 var $close_x = $popup.children().first();
                 $popup.css({ visibility: 'hidden' });
+                $content_child.hide();
             }, 400);
         });
-
-        $(data_behavior('popup_content')).empty();
-        $('body').scrollTop(0);
     });
 });
