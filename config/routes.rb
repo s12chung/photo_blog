93f5a6a310918d0a106ae64af48f06a256cc3e5c
Steve Chung <steve.chung7@gmail.com>
2014-06-24 18:08:04 -0400

    post urls at root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ PhotoBlog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :posts, only: %i[show edit update] do
+  resources :posts, only: %i[show edit update], path: "" do
     member do
       patch :toggle_publish
     end
