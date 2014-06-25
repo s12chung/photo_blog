57c7221bff37186cfee5b637ffb2a19ce6436ca0
Steve Chung <steve.chung7@gmail.com>
2014-06-24 21:37:55 -0400

    cleanup ajax request

--- app/assets/javascripts/behaviors/swipe.js
+++ app/assets/javascripts/behaviors/swipe.js
@@ -4,25 +4,27 @@ Behavior.resize_swipe = function() {
     $swipe.find('li').height(window_height);
     $swipe.find(data_behavior('pinch_zoom')).height(window_height);
 };
-
 $(function() {
     var $swipe = $(data_behavior('swipe'));
-    if ($swipe.length > 0) swipe = new Swipe($swipe[0], {
-        startSlide: parseInt($swipe.data('index')),
-        callback: function(index, li) {
-            var $li = $(li);
-            if (!defined($li.data('loaded'))) {
-                $.get($li.data('path') + ".js");
+    if ($swipe.length > 0) {
+        swipe = new Swipe($swipe[0], {
+            startSlide: parseInt($swipe.data('index')),
+            callback: function (index, li) {
+                var $li = $(li);
+                if (!defined($li.data('loaded'))) {
+                    $.ajax($li.data('path'), { dataType: 'script' });
+                }
+                else {
+                    $li.find(data_behavior('summary_content_container')).css({ opacity: 0, display: 'hidden' });
+                    setTimeout(function () {
+                        $li.find(data_behavior('summary_content_container')).fade_show();
+                    }, 100);
+                }
+                clearTimeout(singleTapTimer);
             }
-            else {
-                $li.find(data_behavior('summary_content_container')).css({ opacity: 0, display: 'hidden' });
-                setTimeout(function() {
-                    $li.find(data_behavior('summary_content_container')).fade_show();
-                }, 100);
-            }
-            clearTimeout(singleTapTimer);
-        }
-    });
-    Behavior.resize_swipe();
-    $(window).resize(Behavior.resize_swipe);
+        });
+
+        Behavior.resize_swipe();
+        $(window).resize(Behavior.resize_swipe);
+    }
 });
