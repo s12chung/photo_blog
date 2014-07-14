e1fe2833243b372b07706992d1c715b0f419b1f4
Steve Chung <steve.chung7@gmail.com>
2014-07-14 13:29:57 -0400

    turn on fastclick

--- app/assets/javascripts/jcontrollers/application_jcontroller.js
+++ app/assets/javascripts/jcontrollers/application_jcontroller.js
@@ -2,6 +2,13 @@ Jcontroller.create('application', {
     html: {
         markdown: function() {
             Jcontroller.find('posts').html.index();
+        },
+        after: function() {
+            if (is_mobile) {
+                window.addEventListener('load', function() {
+                    FastClick.attach(document.body);
+                }, false);
+            }
         }
     }
 });
