436253e541fb4e01ebad1b3f65efdf6b02dba404
Steve Chung <steve.chung7@gmail.com>
2014-07-13 23:26:47 -0400

    huge blockquotes

--- app/models/concerns/has_markdown/post_renderer.rb
+++ app/models/concerns/has_markdown/post_renderer.rb
@@ -8,17 +8,31 @@ module HasMarkdown
       @first = true
     end
 
+    def block_quote(quote)
+      if self.class.short? quote
+        "<blockquote class=\"huge\">#{quote}</blockquote>"
+      else
+        "<blockquote>#{quote}</blockquote>"
+      end
+    end
+
     def paragraph(text)
       if text.index(/---|\u2014/) == 0
         "<cite>#{text}</cite>"
       else
         if @first
           @first = false
-          if text.scan(/[.?!\u2026]/).count < 2
+          if self.class.short? text
             tag = "<p class=\"huge\">#{text}</p>"
           end
         end
         tag || "<p>#{text}</p>"
+      end
+    end
+
+    class << self
+      def short?(text)
+        text.scan(/[.?!\u2026]/).count < 2
       end
     end
   end
